7327d5703113636f1cc3b457d4dd76750b84c0d8
Till Mueller <metjm.mail@gmail.com>
2018-05-21 16:35:59 +0200

    change paths for grpc, add getAccount, getTransactionsToThis, getTransactionsFromThis

--- index.js
+++ index.js
@@ -9,41 +9,64 @@ module.exports = class{
         this.url = config.api_url;
     }
 
+    /*********************************************
+     * ***************** GRPC ********************
+     ********************************************/
     async getAccountProto(address){
-        return await axios.get(this.url + "/getAccount?address=" + address).then(r => tools.accounts.accountFromBase64(r.data));
-    }
-
-    async getAccountObj(address){
-        let proto = await this.getAccountProto(address);
-        return proto.toObject();
+        return await axios.get(this.url + "/grpc/getAccount?address=" + address).then(r => tools.accounts.accountFromBase64(r.data));
     }
 
     async getTransactionsToThisProto(address){
-        return await axios.get(this.url + "/getTransactionsToThis?address=" + address).then(r => tools.transactions.transactionListFromBase64(r.data));
+        return await axios.get(this.url + "/grpc/getTransactionsToThis?address=" + address).then(r => tools.transactions.transactionListFromBase64(r.data));
     }
 
     async getTransactionsFromThisProto(address){
-        return await axios.get(this.url + "/getTransactionsFromThis?address=" + address).then(r => tools.transactions.transactionListFromBase64(r.data));
+        return await axios.get(this.url + "/grpc/getTransactionsFromThis?address=" + address).then(r => tools.transactions.transactionListFromBase64(r.data));
     }
+
+    async getLastBlock(){
+        return await axios.get(this.url + "/grpc/getLastBlock").then(r => tools.blocks.blockFromBase64(r.data));
+    }
+
+    /*********************************************
+     ************ API USING OUR DB ***************
+     ********************************************/
+
+    async getAccount(address){
+        return await axios.get(this.url + "/getAccount?address=" + address).then(x => x.data);
+    }
+
+    async getTransactionsToThis(address){
+        return await axios.get(this.url + "/getTransactionsToThis?address=" + address).then(x => x.data);
+    }
+
+    async getTransactionsFromThis(address){
+        return await axios.get(this.url + "/getTransactionsFromThis?address=" + address).then(x => x.data);
+    }
+
+    /*********************************************
+     *********** TRON FUNCTIONALITY **************
+     ********************************************/
 
     async sendTrx(privateKey, recipient, amount){
         return new Promise(async (resolve, reject) => {
-            let nowBlock = await axios.get(this.url + "/getLastBlock").then(r => tools.blocks.blockFromBase64(r.data));
+            let nowBlock = await this.getLastBlock();
 
             let myAddress = tools.accounts.privateKeyToAddress(privateKey);
 
             let unsigned = await tools.transactions.createUnsignedTransferTransaction(myAddress, recipient, amount, nowBlock);//why does this need an await? maybe i'm too tired
-           let signed = tools.transactions.signTransaction(privateKey, unsigned);
-           let base64Signed = tools.utils.base64EncodeToString(signed.serializeBinary());
+            let signed = tools.transactions.signTransaction(privateKey, unsigned);
+            let base64Signed = tools.utils.base64EncodeToString(signed.serializeBinary());
 
-           let response = await axios.post(this.url + "/broadcastTransaction", qs.stringify({transaction:base64Signed}));
-           let decoded = tools.api.returnFromBase64(response.data).toObject();
-           if(decoded && !decoded.result)
-               decoded.message = Buffer.from(decoded.message, 'base64').toString();
+            let response = await axios.post(this.url + "/broadcastTransaction", qs.stringify({transaction:base64Signed}));
+            let decoded = tools.api.returnFromBase64(response.data).toObject();
+            if(decoded && !decoded.result)
+                decoded.message = Buffer.from(decoded.message, 'base64').toString();
 
-           resolve(decoded);
+            resolve(decoded);
         });
     }
+
 
 }
 
@@ -73,4 +96,24 @@ async function testSend(){
     console.log(sendResponse);
 }
 
-testSend();
+async function testGetLastBlock(){
+    let block = await client.getLastBlock();
+    let transactions = block.toObject();
+
+    console.log(transactions);
+}
+//testSend();
+//testGetLastBlock();
+
+async function testHttpApi(){
+    let account = await client.getAccount("27aqorRGesA7ptBpy2QYTaQBa5TQAg5sGbn");
+    console.log(account);
+    let transactionsTo = await client.getTransactionsToThis("27aqorRGesA7ptBpy2QYTaQBa5TQAg5sGbn");
+    console.log('to:');
+    console.log(transactionsTo);
+    let transactionsFrom = await client.getTransactionsFromThis("27aqorRGesA7ptBpy2QYTaQBa5TQAg5sGbn");
+    console.log('from:');
+    console.log(transactionsFrom);
+}
+
+//testHttpApi();
